2467f3b017aec730b51376ce560acef12cace952
taihuynhz <129383417+taihuynhz@users.noreply.github.com>
2023-04-01 12:19:50 +0700

    SystemTime

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Datetime now is: " + DateTime.Now);
+        }
+    }
+}
